3416f0c11da1fe977f79fe985b4b736f6cffc9f2
carrjes890101 <515199703@qq.com>
2019-12-23 15:50:32 +0800

    test+——01

--- run/run.py
+++ run/run.py
@@ -1,6 +1,5 @@
 import pytest
 import os,time
-import allure
 rootpath = os.path.dirname(os.getcwd())
 date =time.strftime("%Y-%m-%d_%H_%M_%S", time.localtime())
 
@@ -12,7 +11,6 @@ def run_test():
     # print(test_folder)
     pytest.main([test_folder,'--alluredir=%s' %(report_path),'-o log_cli=true -o log_cli_level=INFO'])
     os.system('allure generate %s -o %s/html --clean' %(report_path,allure_report_path))    # 替换为本地的 allure 安装路径
-
 
 
 run_test()
