41cb592cc4b570a298f29f15e4700b1e86bfb978
carrjes890101 <515199703@qq.com>
2019-12-23 15:12:11 +0800

    test+——01

--- cases/test_reg.py
+++ cases/test_reg.py
@@ -13,53 +13,36 @@ card = d['data1']['hospital_card']
 @allure.feature("挂号流程测试")
 class Test_Register(unittest.TestCase):
     @classmethod
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.page = RegisterCase(self.driver)
-        self.base_url = url
+    def setup_class(cls):
+        cls.driver = webdriver.Chrome()
+        cls.page = RegisterCase(cls.driver)
+        cls.base_url = url
 
-    def teardowm(self):
-        self.driver.quit()
+    @classmethod
+    def teardown_class(cls):
+        cls.driver.quit()
 
     @allure.title('测试挂号')
     @allure.story("测试挂号流程")
     def test_register_01(self):
-        self.driver.get(self.base_url)
-        picture = screen_shot(self.driver, "自助机首页")
-        allure.attach.file(picture, "自助机首页", attachment_type=allure.attachment_type.PNG)
+        self.page.home()
         time.sleep(5)
         self.page.register_click()
-        picture = screen_shot(self.driver, "选择就诊卡类型")
-        allure.attach.file(picture, "选择就诊卡类型", attachment_type=allure.attachment_type.PNG)
         time.sleep(5)
         self.page.input_card_click()
-        picture = screen_shot(self.driver, "输入就诊卡")
-        allure.attach.file(picture, "输入就诊卡", attachment_type=allure.attachment_type.PNG)
         time.sleep(7)
         self.page.card_input(card)
         self.page.card_confirm()
-        picture = screen_shot(self.driver, "查询科室")
-        allure.attach.file(picture, "查询科室", attachment_type=allure.attachment_type.PNG)
         time.sleep(7)
         self.page.get_depts()
-        picture = screen_shot(self.driver, "查询就诊日期")
-        allure.attach.file(picture, "查询就诊日期", attachment_type=allure.attachment_type.PNG)
         time.sleep(7)
         self.page.get_date()
-        picture = screen_shot(self.driver, "选择就诊上下午")
-        allure.attach.file(picture, "选择就诊上下午", attachment_type=allure.attachment_type.PNG)
         time.sleep(7)
         self.page.get_am()
-        picture = screen_shot(self.driver, "查询就诊医生")
-        allure.attach.file(picture, "查询就诊医生", attachment_type=allure.attachment_type.PNG)
         time.sleep(7)
         self.page.get_docs()
-        picture = screen_shot(self.driver, "查询剩余号源")
-        allure.attach.file(picture, "查询剩余号源", attachment_type=allure.attachment_type.PNG)
         time.sleep(7)
         self.page.get_regnum()
-        picture = screen_shot(self.driver, "锁号")
-        allure.attach.file(picture, "锁号", attachment_type=allure.attachment_type.PNG)
         time.sleep(5)
         self.page.get_confirm_reg()
 
--- page/register_case.py
+++ page/register_case.py
@@ -27,49 +27,58 @@ class RegisterCase(Page):
     def __init__(self, driver, base_url=url):
         Page.__init__(self, driver, base_url)
 
-    def HomePage(self):
-        print("打开首页:", self.base_url)
-        self.index()
+    @allure.step("-----打开首页-----")
+    def home(self):
+        try:
+            print("打开首页:", self.base_url)
+            picture = screen_shot(self.driver, "自助机首页")
+            allure.attach.file(picture, "自助机首页", attachment_type=allure.attachment_type.PNG)
+            self.index()
+        except Exception as msg:
+            picture = screen_shot(self.driver, "打开首页失败")
+            allure.attach.file(picture, "打开首页失败", attachment_type=allure.attachment_type.PNG)
+            print("-----打开首页失败:{}------".format(msg))
+            raise
 
-    def register(self, card):
-        self.driver.get(self.base_url)
-        picture = screen_shot(self.driver, "自助机首页")
-        allure.attach.file(picture,"自助机首页", attachment_type=allure.attachment_type.PNG)
-        time.sleep(5)
-        self.register_click()
-        picture = screen_shot(self.driver, "选择就诊卡类型")
-        allure.attach.file(picture, "选择就诊卡类型", attachment_type=allure.attachment_type.PNG)
-        time.sleep(5)
-        self.input_card_click()
-        picture = screen_shot(self.driver, "输入就诊卡")
-        allure.attach.file(picture, "输入就诊卡", attachment_type=allure.attachment_type.PNG)
-        time.sleep(7)
-        self.card_input(card)
-        self.card_confirm()
-        picture = screen_shot(self.driver, "查询科室")
-        allure.attach.file(picture, "查询科室", attachment_type=allure.attachment_type.PNG)
-        time.sleep(7)
-        self.get_depts()
-        picture = screen_shot(self.driver, "查询就诊日期")
-        allure.attach.file(picture, "查询就诊日期", attachment_type=allure.attachment_type.PNG)
-        time.sleep(7)
-        self.get_date()
-        picture = screen_shot(self.driver, "选择就诊上下午")
-        allure.attach.file(picture, "选择就诊上下午", attachment_type=allure.attachment_type.PNG)
-        time.sleep(7)
-        self.get_am()
-        picture = screen_shot(self.driver, "查询就诊医生")
-        allure.attach.file(picture, "查询就诊医生", attachment_type=allure.attachment_type.PNG)
-        time.sleep(7)
-        self.get_docs()
-        picture = screen_shot(self.driver, "查询剩余号源")
-        allure.attach.file(picture, "查询剩余号源", attachment_type=allure.attachment_type.PNG)
-        time.sleep(7)
-        self.get_regnum()
-        picture = screen_shot(self.driver, "锁号")
-        allure.attach.file(picture, "锁号", attachment_type=allure.attachment_type.PNG)
-        time.sleep(5)
-        self.get_confirm_reg()
+    # def register(self, card):
+    #     self.driver.get(self.base_url)
+    #     picture = screen_shot(self.driver, "自助机首页")
+    #     allure.attach.file(picture,"自助机首页", attachment_type=allure.attachment_type.PNG)
+    #     time.sleep(5)
+    #     self.register_click()
+    #     picture = screen_shot(self.driver, "选择就诊卡类型")
+    #     allure.attach.file(picture, "选择就诊卡类型", attachment_type=allure.attachment_type.PNG)
+    #     time.sleep(5)
+    #     self.input_card_click()
+    #     picture = screen_shot(self.driver, "输入就诊卡")
+    #     allure.attach.file(picture, "输入就诊卡", attachment_type=allure.attachment_type.PNG)
+    #     time.sleep(7)
+    #     self.card_input(card)
+    #     self.card_confirm()
+    #     picture = screen_shot(self.driver, "查询科室")
+    #     allure.attach.file(picture, "查询科室", attachment_type=allure.attachment_type.PNG)
+    #     time.sleep(7)
+    #     self.get_depts()
+    #     picture = screen_shot(self.driver, "查询就诊日期")
+    #     allure.attach.file(picture, "查询就诊日期", attachment_type=allure.attachment_type.PNG)
+    #     time.sleep(7)
+    #     self.get_date()
+    #     picture = screen_shot(self.driver, "选择就诊上下午")
+    #     allure.attach.file(picture, "选择就诊上下午", attachment_type=allure.attachment_type.PNG)
+    #     time.sleep(7)
+    #     self.get_am()
+    #     picture = screen_shot(self.driver, "查询就诊医生")
+    #     allure.attach.file(picture, "查询就诊医生", attachment_type=allure.attachment_type.PNG)
+    #     time.sleep(7)
+    #     self.get_docs()
+    #     picture = screen_shot(self.driver, "查询剩余号源")
+    #     allure.attach.file(picture, "查询剩余号源", attachment_type=allure.attachment_type.PNG)
+    #     time.sleep(7)
+    #     self.get_regnum()
+    #     picture = screen_shot(self.driver, "锁号")
+    #     allure.attach.file(picture, "锁号", attachment_type=allure.attachment_type.PNG)
+    #     time.sleep(5)
+    #     self.get_confirm_reg()
 
     @allure.step("-----点击门诊挂号-----")
     def register_click(self):
@@ -79,55 +88,119 @@ class RegisterCase(Page):
     @allure.step("-----点击输入就诊卡号-----")
     def input_card_click(self):
         print("-----点击输入就诊卡号-----")
+        picture = screen_shot(self.driver, "选择就诊卡类型")
+        allure.attach.file(picture, "选择就诊卡类型", attachment_type=allure.attachment_type.PNG)
         self.click(self.enter_card)
 
     @allure.step("-----输入就诊卡号-----")
     def card_input(self, card_num):
         print("-----输入就诊卡号------")
+        picture = screen_shot(self.driver, "输入就诊卡")
+        allure.attach.file(picture, "输入就诊卡", attachment_type=allure.attachment_type.PNG)
         return self.input_text(self.input_card, card_num)
 
     @allure.step("-----点击确认-----")
     def card_confirm(self):
-        print("-----点击确认-----")
-        return self.click(self.confirm_btn)
+        try:
+            print("-----点击确认-----")
+            return self.click(self.confirm_btn)
+        except Exception as msg:
+            picture = screen_shot(self.driver, "点击确认失败")
+            allure.attach.file(picture, "点击确认失败", attachment_type=allure.attachment_type.PNG)
+            print("-----点击确认失败:{}------".format(msg))
+            raise
 
     @allure.step("-----查看就诊科室-----")
     def get_depts(self):
-        print("-----查看就诊科室------")
-        self.get_elements(self.dept)
+        try:
+            picture = screen_shot(self.driver, "查询科室")
+            allure.attach.file(picture, "查询科室", attachment_type=allure.attachment_type.PNG)
+            print("-----查看就诊科室------")
+            self.get_elements(self.dept)
+        except Exception as msg:
+            picture = screen_shot(self.driver, "查询科室失败")
+            allure.attach.file(picture, "查询科室失败", attachment_type=allure.attachment_type.PNG)
+            print("-----查询科室失败:{}------".format(msg))
+            raise
 
     @allure.step("-----获取就诊卡姓名-----")
     def get_username(self):
-        print("-----获取就诊卡姓名-----")
-        return self.get_text(self.username)
+        try:
+            print("-----获取就诊卡姓名-----")
+            return self.get_text(self.username)
+        except Exception as msg:
+            picture = screen_shot(self.driver, "获取就诊卡姓名失败")
+            allure.attach.file(picture, "获取就诊卡姓名失败", attachment_type=allure.attachment_type.PNG)
+            print("-----获取就诊卡姓名失败:{}------".format(msg))
+            raise
 
     @allure.step("-----查看时间-----")
     def get_date(self):
-        print("-----查看时间------")
-        self.get_elements(self.date)
+        try:
+            print("-----查看时间------")
+            picture = screen_shot(self.driver, "查询就诊日期")
+            allure.attach.file(picture, "查询就诊日期", attachment_type=allure.attachment_type.PNG)
+            self.get_elements(self.date)
+        except Exception as msg:
+            picture = screen_shot(self.driver, "查询就诊日期失败")
+            allure.attach.file(picture, "查询就诊日期失败", attachment_type=allure.attachment_type.PNG)
+            print("-----查询就诊日期失败:{}------".format(msg))
+            raise
 
     @allure.step("-----选择上下午-----")
     def get_am(self):
-        print("-----选择上下午------")
-        self.get_elements(self.am_pm)
+        try:
+            print("-----选择上下午------")
+            picture = screen_shot(self.driver, "选择就诊上下午")
+            allure.attach.file(picture, "选择就诊上下午", attachment_type=allure.attachment_type.PNG)
+            self.get_elements(self.am_pm)
+        except Exception as msg:
+            picture = screen_shot(self.driver, "选择就诊上下午失败")
+            allure.attach.file(picture, "选择就诊上下午失败", attachment_type=allure.attachment_type.PNG)
+            print("-----选择就诊上下午失败:{}------".format(msg))
+            raise
 
     @allure.step("-----查看就诊医生-----")
     def get_docs(self):
-        print("-----查看就诊医生------")
-        self.get_elements(self.doc_list)
+        try:
+            picture = screen_shot(self.driver, "查询就诊医生")
+            allure.attach.file(picture, "查询就诊医生", attachment_type=allure.attachment_type.PNG)
+            print("-----查看就诊医生------")
+            self.get_elements(self.doc_list)
+        except Exception as msg:
+            picture = screen_shot(self.driver, "查询就诊医生失败")
+            allure.attach.file(picture, "查询就诊医生失败", attachment_type=allure.attachment_type.PNG)
+            print("-----查询就诊医生失败:{}------".format(msg))
+            raise
 
     @allure.step("-----查看就诊号源-----")
     def get_regnum(self):
-        print("-----查看就诊号源------")
-        self.get_elements(self.reg_num)
+        try:
+            print("-----查看就诊号源------")
+            self.get_elements(self.reg_num)
+            picture = screen_shot(self.driver, "查询剩余号源")
+            allure.attach.file(picture, "查询剩余号源", attachment_type=allure.attachment_type.PNG)
+        except Exception as msg:
+            picture = screen_shot(self.driver, "查询剩余号源失败")
+            allure.attach.file(picture, "查询剩余号源失败", attachment_type=allure.attachment_type.PNG)
+            print("-----查询剩余号源失败:{}------".format(msg))
+            raise
 
     @allure.step("-----确认挂号信息-----")
     def get_confirm_reg(self):
-        print("-----确认挂号信息------")
-        return self.get_text(self.confirm_reg)
+        try:
+            picture = screen_shot(self.driver, "锁号")
+            allure.attach.file(picture, "锁号", attachment_type=allure.attachment_type.PNG)
+            print("-----确认挂号信息------")
+            return self.get_text(self.confirm_reg)
+        except Exception as msg:
+            picture = screen_shot(self.driver, "锁号失败")
+            allure.attach.file(picture, "锁号失败", attachment_type=allure.attachment_type.PNG)
+            print("-----锁号失败:{}------".format(msg))
+            raise
 
 
 if __name__ == '__main__':
     driver = webdriver.Chrome()
     test = RegisterCase(driver)
-    test.register(card)
+
